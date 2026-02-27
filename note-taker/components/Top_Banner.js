db6a9e3ed29f3a360db685372a36157b64c61756
DaniyalNaeem2005 <danunaeem@gmail.com>
2026-02-27 21:05:44 +0500

    Basic skeleton of the application

---
+++ note-taker/components/Top_Banner.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { View, Text, StyleSheet, Image, SafeAreaView } from "react-native";
+
+export default function TopBanner() {
+  return (
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.banner}>
+        <Image
+          source={require("../assets/notes_logo.png")}
+          style={styles.logo}
+        />
+        <Text style={styles.title}>TrustBite</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  safeArea: {
+    backgroundColor: "#9E090F",
+  },
+  banner: {
+    width: "100%",
+    height: 50,
+    backgroundColor: "#FFF4E2",
+    flexDirection: "row",
+    alignItems: "center",
+    paddingHorizontal: 15,
+  },
+  logo: {
+    width: 30,
+    height: 30,
+    resizeMode: "contain",
+    marginRight: 10,
+  },
+  title: {
+    color: "#000",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+});
